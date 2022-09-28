12cdfc05c0588db9ea699362169f0c8ae4a05a5e
_Nastasia_ <nastya.svirid@mail.ru>
2022-09-28 14:49:29 +0300

    Lesson 3 Exercise 1-3

---
+++ Lesson-3/index.js
@@ -0,0 +1,30 @@
+// Exercise 1
+for (let i = 1; i <= 50; i++ ){
+    console.log (i)
+}
+for (let j = 35; j >=8; j--){
+    console.log (j)
+}
+// Exercise 2
+let x = 90;
+while (x>11){
+    x--;
+    console.log(x)
+}
+// Exercise 3
+let sum = 0
+for (let y = 0; y<=100; y++){
+sum = sum+y;
+}
+console.log(sum)
+// Exercise 4
+
+// Exercise 5
+
+
+
+// Exercise 6
+// Exercise 7
+// Exercise 8
+// Exercise 9
+// Exercise 10
