cad88634733c93f6fff19e0402cb917e53360606
_Nastasia_ <nastya.svirid@mail.ru>
2022-09-28 16:44:16 +0300

    Add exercise

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -18,13 +18,40 @@ sum = sum+y;
 }
 console.log(sum)
 // Exercise 4
-
+let s = 0
+for (let y = 0; y<=5; y++){
+s = s+y;
+}
+console.log(s)
 // Exercise 5
+for (let h = 8; h <=56; h=h+2){
+    console.log (h)
+}
+let h = 6;
+ while (h<=54){
+    h=h+2;
+   console.log(h)
+ }
 
-
-
-// Exercise 6
+// // Exercise 6
+// let r= 0;
+// for (let m = 2; m<=10; m++){
+//     for (let p = 0; p<=10; p++){
+// r= m*p;
+// console.log(m*p r)
+//     }
+// }
 // Exercise 7
+
+for (let n = 1000; n>50; ) {
+// num = 5
+    n /= 2;
+    if (n<50){
+        console.log( n );}
+    }
+
 // Exercise 8
 // Exercise 9
+let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57'
+
 // Exercise 10
