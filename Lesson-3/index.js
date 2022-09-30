802033f14ca4a8475d1473b2f334ad10c50b49f5
_Nastasia_ <nastya.svirid@mail.ru>
2022-09-30 11:58:43 +0300

    Add lessons 4-5

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -1,89 +1,91 @@
-// // Exercise 1
-// for (let i = 1; i <= 50; i++ ){
-//     console.log (i)
-// }
-// for (let j = 35; j >=8; j--){
-//     console.log (j)
-// }
-// // Exercise 2
-// let x = 90;
-// while (x>11){
-//     x--;
-//     console.log(x)
-// }
-// // Exercise 3
-// let sum = 0
-// for (let y = 0; y<=100; y++){
-// sum = sum+y;
-// }
-// console.log(sum)
-// // Exercise 4
-// let s = 0
-// for (let y = 0; y<=5; y++){
-// s = s+y;
-// }
-// console.log(s)
-// // Exercise 5
-// for (let h = 8; h <=56; h=h+2){
-//     console.log (h)
-// }
-// let h = 6;
-//  while (h<=54){
-//     h=h+2;
-//    console.log(h)
-//  }
+// Exercise 1
+for (let i = 1; i <= 50; i++ ){
+    console.log (i)
+}
+for (let j = 35; j >=8; j--){
+    console.log (j)
+}
+// Exercise 2
+let x = 90;
+while (x>11){
+    x--;
+    console.log(x)
+}
+// Exercise 3
+let sum = 0
+for (let y = 0; y<=100; y++){
+sum = sum+y;
+}
+console.log(sum)
+// Exercise 4
+let s = 0
+for (let y = 0; y<=5; y++){
+s = s+y;
+}
+console.log(s)
+// Exercise 5
+for (let h = 8; h <=56; h=h+2){
+    console.log (h)
+}
+let h = 6;
+ while (h<=54){
+    h=h+2;
+   console.log(h)
+ }
 
-// // Exercise 6
-// let r = 0;
-// for (let m = 1; m < 10; m++) {
-//     for (let p = 1; p < 10; p++) {
-//         r = m * p;
-//         console.log(m + '*' + p + '=' +r)
-//     }
-// }
+// Exercise 6
+let r = 0;
+for (let m = 1; m < 10; m++) {
+    for (let p = 1; p < 10; p++) {
+        r = m * p;
+        console.log(m + '*' + p + '=' +r)
+    }
+}
 
 
-// // Exercise 7
-// num = 0;
-// for (let n = 1000; n > 50; ) {
-//   num += 1;
-//   n /= 2;
-//   if (n < 50) {
-//     console.log(n);
-//     console.log(num);
-//   }
-// }
+// Exercise 7
+num = 0;
+for (let n = 1000; n > 50; ) {
+  num += 1;
+  n /= 2;
+  if (n < 50) {
+    console.log(n);
+    console.log(num);
+  }
+}
 
-// // Exercise 8
-// let e = 0,
-//     o = 0,
-//     ch;
+// Exercise 8
+let e = 0,
+    o = 0,
+    ch;
 
-// while ((ch = prompt("Введите число", 0)) !== "0" && ch != null) {
-//    if (Number(ch)){
-//     ++e;
-//     o += +ch; 
-//    } else{
-//     alert ('Ошибка')
-//     break;
-//    }
-// }
+while ((ch = prompt("Введите число", 0)) !== "0" && ch != null) {
+   
 
-// alert("Чисел введено: " + e + "\n Сумма чисел: " + o
-//     + "\n Среднее арифметическое: " + (o / e));
+    ++e;
+    o += +ch; // Перед ch надо для числа ставить плюс
+    // if (ch == " "){
+    //     alert("Только числа")
+        
+    // }
+   
+}
+
+alert("Чисел введено: " + e + "\n Сумма чисел: " + o
+    + "\n Среднее арифметическое: " + (o / e));
     
-// // Exercise 9
-// let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57'
-// let cur='', min, max;
-// for (let i = 0; i < str.length; i++) {
-//   cur += str[i];
-//   if (+str[i] && !+str[i+1]) {
-//     if (typeof min !== 'number' || cur < +min) min = +cur;
-//     if (typeof max !== 'number' || cur > +max) max = +cur;
-//     cur = '';
-//   }
-// }
-// console.log('Min:', min, 'Max:', max)
+// Exercise 9
+let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57'
+let cur='', min, max;
+for (let i = 0; i < str.length; i++) {
+  cur += str[i];
+  if (+str[i] && !+str[i+1]) {
+    if (typeof min !== 'number' || cur < +min) min = +cur;
+    if (typeof max !== 'number' || cur > +max) max = +cur;
+    cur = '';
+  }
+}
+console.log('Min:', min, 'Max:', max)
 
 // Exercise 10
 let n = "12357821";
@@ -100,8 +102,7 @@ function sum(array) {
   for (i = 0; i < array.length; i++) {
     s += array[i];
   }
-  
+
   return s;
- 
 }
-consol.log (sum(array))
+console.log(s);
---
+++ Lesson-4/index.js
@@ -0,0 +1,53 @@
+// //Exercise 1
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i<arr.length; i++){
+//    document.write (arr [i
+//    ]+ "<br>");
+// }
+
+//  //Exercise 2
+
+//  let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+
+//  for (let i = 0; i<arr.length; i++){
+//  if (arr [i] >-10 && arr [i] <-3){
+// console.log(arr [i]);}
+//  }
+
+// // Exercise 3
+
+// let arr = [];
+// result = 0;
+// for (let i = 23; i <=57; i++) {
+//     arr.push(i);
+
+// }
+
+// for (let i = 0; i <arr.length; i++){
+//         result += arr[i]
+//  }
+// console.log(arr)
+// console.log(result)
+
+//WHILE
+
+// // Exercise 4
+
+// let arr = ['10', '20', '30', '50', '235', '3000']
+// for (let i = 0; i < arr.length; i++) {
+//     let num = String(arr[i]);
+//     let char = num[0];
+//     if (char == 1 || char == 2 || char == 5) {
+//       console.log(num);
+//     }
+//   }
+
+//Exercise 5
+
+let arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+for (let i =0; i<arr.length; i++){
+
+    console.log(arr)
+
+}
+
