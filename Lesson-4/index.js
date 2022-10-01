425453d6f14786611d0ee0986de78ec0e475ba0e
unknown <nastya.svirid@mail.ru>
2022-10-01 23:28:23 +0300

    Homework lesson 4

--- Lesson-4/index.js
+++ Lesson-4/index.js
@@ -11,62 +11,131 @@
 
 //  for (let i = 0; i<arr.length; i++){
 //  if (arr [i] >-10 && arr [i] <-3){
-// console.log(arr [i]);}
+// document.write(arr [i]);}
 //  }
 
 // // Exercise 3
 
 // let arr = [];
 // result = 0;
-// for (let i = 23; i <=57; i++) {
-//     arr.push(i);
-
+// for (let j = 23; j <=57; j++) {
+//     arr.push(j);
+//     result += j
 // }
 
-// for (let i = 0; i <arr.length; i++){
-//         result += arr[i]
-//  }
+// // for (let i = 0; i <arr.length; i++){                   //сократили эту запись
+// //         result += arr[i]
+// //  }
 // console.log(arr)
 // console.log(result)
 
-//WHILE
+// // WHILE
+// let arr2 = [];
+// result2 = 0;
+// let y = 23;
+// while (y<=57){
+
+//     arr2.push(y);
+//     result2 += y
+//     y++;
+
+// }
+// console.log(arr2)
+// console.log(result2)
 
 // // Exercise 4
 
 // let arr = ['10', '20', '30', '50', '235', '3000']
 // for (let i = 0; i < arr.length; i++) {
-//     let num = String(arr[i]);
+//     let num = arr[i];
 //     let char = num[0];
 //     if (char == 1 || char == 2 || char == 5) {
-//       console.log(num);
+//       document.write(num+'<br>');
 //     }
 //   }
 
-//Exercise 5
+// //Exercise 5
 
 // let arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 // for (let i =0; i<arr.length; i++){
-
-//     console.log(arr[i])
-
+// if (arr [i] == 'Saturday' || arr [i] == 'Sunday'){
+//     document.write(arr[i].bold()+'<br>')
+// } else{
+//     document.write(arr[i]+'<br>')
+// }
 // }
 
-//Exercise 6
+// //Exercise 6
+// let arr = [1, 2, 3, 4, 5, 6];
+// arr.push (95);
+// console.log(arr);
+// let i = arr.length;
+// console.log(arr[i-1])
 
-//Exercise 7
+// //Exercise 7
+// let arr = [];
+// let z,
+// n= 0;
+// while ((z = prompt ('Введите число')) !== '' && z !== '0'){
+//     n =+z;
+// arr.push(n);
+// }
+// console.log(arr);
+// arr.sort(function(a,b){
+//     return a-b});
+//     console.log(arr);
+
 
 // //Exercise 8
 // let arr = [12, false, 'Текст', 4, 2, -5, 0];
 
 // console.log(arr.reverse());
-//Exercise 9
-let arr =  [5, 9, 21, , , 9, 78, , , , 6];
-num = 0;
-for ( let i = 0; i<arr.length; i++){
-   
-    if (i !== 'number'){
-        num += 1
-        console.log(num)
-    }
-}
+
+// //Exercise 9
+// let arr = [5, 9, 21, , , 9, 78, , , , 6];
+// num = 0;
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (arr[i] ==null) {
+//         num++
+//     }
+// }
+// console.log(num)
+
 //Exercise 10
+
+// let arr = [1, 8, 0, 13, 76, 8, 7, 0, 22, 0, 2, 3, 2];
+// let arr1 = [];
+// let sum =0;
+//     n = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] = 0 && n !==2 || sum !==0) {
+//         n++
+//         i++
+//         sum += arr[i];
+        
+//     }
+    
+//     n = 0;
+// }
+// console.log(sum);
+
+
+let arr = [1, 8, 0, 13, 76, 8, 7, 0, 22, 0, 2, 3, 2];
+let sum = 0;
+n = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] = 0 && n !== 1 && sum !== 0) {
+    n++;
+    
+    if (arr[i+1] !== 0) {
+      sum += arr[i+1];
+     i++
+    }
+   
+  }
+  n = 0;
+}
+console.log(sum);
--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -1,11 +1,39 @@
 //Exercise 1
+function h1(a, b, c) {
 
+    console.log(a - b / c)
+}
+h1(12, 2, 3)
 //Exercise 2
+function h2(a) {
 
+    console.log(a**2 , a**3)
+}
+h2(2)
 //Exercise 3
-
+function min(a,b) {
+   if (a<b){
+    console.log(a)
+   } else{ 
+    console.log(b)
+   }
+}
+function max(a,b) {
+    if (a>b){
+        console.log(a)
+       } else{ 
+        console.log(b)
+       }
+}
+min(2,4)
+max(5,9)
 //Exercise 4
-
+function create (lenght){
+    let arr = [];
+    for (let i = 0; i<lenght; i++){
+        arr.push(i);}
+    }
+    create(6)
 //Exercise 5
 
 //Exercise 6
